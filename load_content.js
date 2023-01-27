73c503060d2b582c54120905cb5a0b3ff26df334
ESD-SystemsAutomation <123769026+ESD-SystemsAutomation@users.noreply.github.com>
2023-01-27 11:26:25 -0800

    Add files via upload

---
+++ load_content.js
@@ -0,0 +1,3 @@
+var node = document.createElement('div')
+node.innerHTML = '<p>Network Security Key: EdmondsSD</p><p>Passcode: 843027</p><br /><p>Generated Thursday 01/26/2023 2:00 AM</p>'
+document.appendChild(node)
